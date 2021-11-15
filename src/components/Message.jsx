ffa0a756830d1146c3703488dff3aceda0df1d66
savinsv <razi@yandex.ru>
2021-11-15 20:37:25 +0300

    Немного стилизовал

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,13 +1,14 @@
 import React from "react";
+import './Message.css';
 
 const Message = (props) => (
     
     (props.messName) ? 
-    <div>
+    <div className="Message-text-good">
         Приветствуем Вас, {props.messName}!
     </div>
     : 
-    <div>
+    <div className="Message-text-non">
         Пока поприветствовать некого, а жаль...
     </div>
 );
